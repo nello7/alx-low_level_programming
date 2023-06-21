e0983547124223f72ccef8f423d338e8cbf01794
nello7 <sagoepaul213@gmail.com>
2023-06-21 16:52:18 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,13 +8,16 @@
   *Return: 1 for letters. 0 for the rest.
   */
 int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+
 	}
-if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-{
-	return (1);
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
 }
-else
-{
-	return (0);
-}
-_putchar('\n');
+
