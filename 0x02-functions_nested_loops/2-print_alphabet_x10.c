ca229e1322d74aee4041ceb87d76ab64f7fb77fa
nello7 <sagoepaul213@gmail.com>
2023-06-21 11:38:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
+char j = 'a';
 	int i;
-	int j;
 
-	for (j = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 97; j <= 122; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n';);
+		_putchar('\n');
 	}
 }
